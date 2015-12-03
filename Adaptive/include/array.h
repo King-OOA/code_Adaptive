20dd23e619dbaca34549d3754049814f98385a87
pengzhan <284270220@qq.com>
2015-12-03 21:53:56 +0800

    修改array.h

--- Adaptive/include/array.h
+++ Adaptive/include/array.h
@@ -10,19 +10,21 @@ typedef struct single_str_t {
   Char_t str[];
 } Single_Str_t;
 
-typedef struct str_elmt {
-  Flag_t pat_end_flag; 
-  union {
-    Char_t buf[POINTER_SIZE];
-    Char_t *p;
-  } str;
-  Expand_Node_t expand_node;
-} Str_Elmt_t;
+/* typedef struct str_elmt { */
+/*   Flag_t pat_end_flag;  */
+/*   union { */
+/*     Char_t buf[POINTER_SIZE]; */
+/*     Char_t *p; */
+/*   } str; */
+/*   Expand_Node_t expand_node; */
+/* } Str_Elmt_t; */
 
 typedef struct str_array {
   Pat_Num_t str_num;
   Pat_Len_t str_len;
-  Str_Elmt_t array[];
+  Expand_Node_t *expand_nodes;
+  Flag_t is_pat_end[NUM_TO_BUILD_ARRAY/8+1];
+  Char_t array[];
 } Str_Array_t;
 
 void build_array(Expand_Node_t *expand_node, Pat_Num_t str_num, Pat_Len_t str_len);
